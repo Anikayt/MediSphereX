9ac7180d1643e73cd9655a05dfc8f924a69c1813
Aniket Mishra <138378049+Anikayt@users.noreply.github.com>
2024-08-12 20:30:02 +0530

    Fixing CORS error 2

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,13 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({ path: "./config.env" });
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-   });
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
